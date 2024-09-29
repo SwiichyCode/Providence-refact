77027efa0431c5a8b892ffd7ab74ab73cb8d9396
SwiichyCode <adlpromail@gmail.com>
2024-09-30 01:30:00 +0200

    refact: translate footer

--- src/app/providence/_components/footer.tsx
+++ src/app/providence/_components/footer.tsx
@@ -24,7 +24,7 @@ export const Footer = () => {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2024 Providence, Inc. Tous droits réservés.
           </p>
         </div>
       </div>
